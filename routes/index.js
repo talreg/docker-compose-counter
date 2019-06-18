dea7e4191cbb3d935741804301d395422c4af8ae
Tal Riginiano <tal@talreg.com>
2019-06-18 09:58:21 -0400

    fix redis connection

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,25 @@ const ServiceResolver=require("../service-resolver")
 const defaultRoute="/"
 
 let serviceResolver=new ServiceResolver()
+let err_message=""
+
 router.get(defaultRoute, function(req, res, next) {
-		const client = redis.createClient(6379,serviceResolver.getDNSforService("redis"));
-		client.get("counter",function(err,value){
+	const client = redis.createClient(6379,serviceResolver.getDNSforService("redis"));
+	client.on("error", err=> {
+		err_message=`${serviceResolver.getDNSforService("redis")} server can't be found. ${err}`
+	});
+	client.on('connect', function() {
+	    err_message=""
+	});
+	if (err_message!=""){
+		res.send(err_message)
+		return
+	}
+	client.get("counter",function(err,value){
+		if(err){
+			res.send("redis error:"+err)
+			return
+		}
 		if (!value){
 			value=1;
 		}
